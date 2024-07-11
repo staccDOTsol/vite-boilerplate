6c027868b120e42462aa4bddf504ed860a2ad9a2
Jarett Dunn <jarettdunn@Jaretts-Laptop.local>
2024-07-12 00:36:15 +0100

    redundant imports relieved; until such time as token groups

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    nodePolyfills({
+      include: ['buffer'],
+    }),
     react(),
-    basicSsl(),
-    NodeModulesPolyfillPlugin({
-      // To polyfill Buffer globally
-      buffer: true
-    })
+    basicSsl()
   ],
   build: {
     outDir: './docs'
+  },
+  resolve: {
+    alias: {
+      // This is required for the polyfill to work
+      buffer: 'buffer',
+    },
   },
   base: '/vite-boilerplate/'
 });
