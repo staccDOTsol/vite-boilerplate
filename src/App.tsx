513b931e890b17eec53cec6c18a81c51ddad7e75
Jarett Dunn <jarettdunn@Jaretts-Laptop.local>
2024-07-12 05:20:53 +0100

    redundant imports relieved; until such time as token groups

--- src/App.tsx
+++ src/App.tsx
@@ -194,7 +194,7 @@ const App = () => {
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
   };
 
-  const isWinner = wallet && lastBuyer === wallet.account.address || lastBuyer == null;
+  const isWinner = wallet && lastBuyer === wallet.account.address || lastBuyer == '';
   const canClaimWin = timeLeft === 0 && isWinner;
 
   return (
