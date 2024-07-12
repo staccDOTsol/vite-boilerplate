6667aef4437ed12d5bbef510e9f6467fceb3cb55
Jarett Dunn <jarettdunn@Jaretts-Laptop.local>
2024-07-12 05:52:18 +0100

    redundant imports relieved; until such time as token groups

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
   const wallet = useTonWallet();
   const [tonConnectUI] = useTonConnectUI();
   const [client, setClient] = useState<TonClient | undefined>();
-  const contractAddress = 'EQDnXZ0IFFxCVPfWc6QZq1SrgxPrQANseL44wXfcSDwn9f0c';
+  const contractAddress = 'EQCfoKyRAZsc_mUyk7XS7VCi4r3qcuuLduPklrLpdvNhwKCs';
 
   useEffect(() => {
     const initTonClient = async () => {
