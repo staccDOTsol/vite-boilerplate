6b55bf151ad29050bbe720af851f62f76958c5c1
Jarett Dunn <jarettdunn@Jaretts-Laptop.local>
2024-07-12 07:13:50 +0100

    redundant imports relieved; until such time as token groups

--- src/App.tsx
+++ src/App.tsx
@@ -67,9 +67,9 @@ const App = () => {
 
           try {
             const keyPriceResult = await contract.get('get_key_price', []);
-            setKeyPrice(Number(fromNano(keyPriceResult.stack.readBigNumber())) + 0.4);
+            setKeyPrice(Number(fromNano(keyPriceResult.stack.readBigNumber())) + 0.666);
           } catch (error) {
-            setKeyPrice(0.4)
+            setKeyPrice(0.666)
             console.error('Error fetching key price:', error);
           }
 
@@ -155,7 +155,7 @@ const App = () => {
         messages: [
           {
             address: contractAddress,
-            amount: toNano('0.4').toString(), // Small amount for gas
+            amount: toNano('0.666').toString(), // Small amount for gas
             payload: beginCell()
               .storeUint(0x595f07bc, 32) // op code for burn
               .storeUint(0, 64) // query_id
@@ -184,7 +184,7 @@ const App = () => {
         messages: [
           {
             address: contractAddress,
-            amount: toNano('0.4').toString(), // Small amount for gas
+            amount: toNano('0.666').toString(), // Small amount for gas
             payload: beginCell()
               .storeUint(2, 32) // op code for claim_win
               .storeUint(0, 64) // query_id
